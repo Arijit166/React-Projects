c901804c9df07f21b11fc849b66efc0fb9b928e8
Arijit166 <arijit.sarkar7156@gmail.com>
2025-08-12 01:41:28 +0530

    Second Project

---
+++ Second-Project/src/Hello.jsx
@@ -0,0 +1,8 @@
+function Hello(){
+    let number=456
+    let fullName=()=>{
+        return "Arijit Sarkar"
+    }
+    return <h3>Hello! This is the future speaking. I am your {fullName()} and my number is {number}</h3>
+}
+export default Hello;
