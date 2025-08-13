7ed26c5c886306a8fa7b91c15409e0efc1827141
Arijit166 <arijit.sarkar7156@gmail.com>
2025-08-14 00:36:18 +0530

    Calculator

---
+++ Seventh-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Display from "./components/Display"
+import ButtonsContainer from "./components/ButtonsContainer"
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <ButtonsContainer />
+    </div>
+  )
+}
+
+export default App
