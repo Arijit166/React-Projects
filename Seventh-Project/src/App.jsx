a92c4ec52d2b767d3a370a1faa148f79dce305b8
Arijit166 <arijit.sarkar7156@gmail.com>
2025-08-16 00:58:36 +0530

    Completed calculator project

--- Seventh-Project/src/App.jsx
+++ Seventh-Project/src/App.jsx
@@ -2,11 +2,25 @@ import styles from "./App.module.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Display from "./components/Display"
 import ButtonsContainer from "./components/ButtonsContainer"
+import {useState} from "react"
 function App() {
+  const [calVal,setcalVal]=useState("")
+  const onButtonClick=(buttonText)=>{
+    if(buttonText==="C"){
+      setcalVal("")
+    }else if (buttonText==="="){
+      const result=eval(calVal);
+      setcalVal(result)
+    }else{
+      const newDisplayValue=calVal+buttonText;
+      setcalVal(newDisplayValue)
+    }
+
+  }
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonsContainer />
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick}/>
     </div>
   )
 }
